6ae26fd6c5ec5329dba6ffc3c9fc0d9e131246f6
Talha Güller <talhaguller7@gmail.com>
2024-04-05 10:23:21 +0300

    BernoulliMap

---
+++ bernoulliMap.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Bernoulli haritası fonksiyonu
+def bernoulli_map(x, r):
+    return r * x * (1 - x)
+
+# Haritayı çizmek için fonksiyon
+def plot_bernoulli_map(r, iterations=1000):
+    x = np.linspace(0, 1, 1000)
+    y = bernoulli_map(x, r)
+
+    plt.figure(figsize=(8, 6))
+    plt.plot(x, y, 'r-', lw=2)
+    plt.plot([0, 1], [0, 1], 'k--', lw=1)
+    plt.title(f'Bernoulli Haritası (r={r})')
+    plt.xlabel('x')
+    plt.ylabel('f(x)')
+    plt.grid(True)
+    plt.show()
+
+# Harita için r değerini seçin (0 ile 4 arasında bir değer olmalı)
+r = 3.6
+
+plot_bernoulli_map(r)
